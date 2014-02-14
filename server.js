20ac7c5026f33bebf965b901f2d932bf94706a0b
Jeremy Steward <jeremy@thatgeoguy.ca>
2014-02-14 16:21:13 -0700

    Changed mustache templating to mustache files

--- server.js
+++ server.js
@@ -12,12 +12,12 @@ var express = require('express'),
 	path = require('path');
 
 var app = express();
-app.engine('html', mustacheExpress());
+app.engine('mustache', mustacheExpress());
 
 // all environments
 app.set('port', process.env.PORT || 8000);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'html');
+app.set('view engine', 'mustache');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
