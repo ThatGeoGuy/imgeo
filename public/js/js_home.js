98e8629583334bc69297f34fa1a290c50b430637
Laura Norman <lauranorman1@gmail.com>
2014-03-10 14:57:50 -0600

    css and js files

---
+++ public/js/js_home.js
@@ -0,0 +1,22 @@
+/*
+ * imgeo Search Functionality
+ * File:         js_home.js
+ * Author:       Laura Norman
+ * Date:         2014-03-04
+ * Description:  
+ */
+
+$(document).ready(function() {
+	var initialize = function() {
+		var mapOptions = {
+		  zoom: 2,
+		  mapTypeId: google.maps.MapTypeId.SATELLITE,
+		  center: new google.maps.LatLng(39,-36),
+		  panControl: false,
+		  scaleControl: false,
+		  zoomControl: false
+		};
+		var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+	}
+	initialize();
+});
---
+++ public/js/js_results.js
@@ -0,0 +1,23 @@
+/*
+ * imgeo Search Functionality
+ * File:         imgeoSearch.js
+ * Author:       Jeremy Steward
+ * Date:         2014-02-20 @ 13:34:55
+ * Description:  Provides the functionality for the search bar on the home page and in the navbar on other pages
+ */
+
+$(document).ready(function() {
+	var initialize = function() {
+		var mapOptions = {
+		  zoom: 1,
+		  mapTypeId: google.maps.MapTypeId.SATELLITE,
+		  center: new google.maps.LatLng(39,-36),
+		  panControl: false,
+		  scaleControl: false,
+		  zoomControl: false
+		};
+		var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+	}
+	initialize();
+	});
+
