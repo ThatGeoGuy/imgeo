0f0c7f5879534a91171cff82a5a80525da0eb0ff
Jeremy Steward <jeremy@thatgeoguy.ca>
2014-03-19 10:50:08 -0600

    getHandlers.js did not correctly merge over

--- routes/getHandlers.js
+++ routes/getHandlers.js
@@ -6,18 +6,67 @@
  * Description : Implements the request handlers for each page on the server
  */
 
-authors = [ 
+var url = require("url"),
+	fs  = require("fs");
+
+var authors = [ 
 	{ "name": "Jeremy Steward" },
 	{ "name": "Laura Norman" }
-]
+];
+
+imageExtensions = [
+	".jpg",
+	".jpeg",
+	".png",
+	".gif",
+	".tif",
+	".tiff"
+];
 
 module.exports = function(app) { 
 	app.get('/', function(req, res) {
-		template = {
-			"description": "Homepage for the IMGEO website",
-			"authors": authors,
-			"index": true, 
+		templateParameters = {
+			"description" : "Homepage for the IMGEO website",
+			"authors"     : authors,
+			"index"       : true,
 		};
-		res.render('index.html', template);
+		res.render('index.html', templateParameters);
 	}); 
+
+	app.get('/upload', function(req, res) { 
+		templateParameters = { 
+			"description" : "Upload your images to the IMGEO service!",
+			"authors"     : authors,
+			"upload"      : true,
+		};
+		res.render('upload.html', templateParameters);
+	});
+
+	app.get('/image/*', function(req, res) {
+		var path = url.parse(req.url).pathname; 
+		path = path.split('/').filter(function(e) { 
+			return e.length > 0; 
+		});
+		path.slice(1);
+
+		var imageString = false; 
+		for(var i = 0; i < imageExtensions.length; ++i) {
+			if(path[1].slice(path[1].length - 4) === imageExtensions[i]) {
+				imageString = "/img/" + path[1]; 
+			}
+		}
+
+		if(imageString && fs.existsSync("public" + imageString)) { 
+			templateParameters = { 
+				"description": "Image result - " + imageString,
+				"authors" : authors, 
+				"imageUrl" : imageString,
+				"pageUrl": app.get('FQDN') + req.originalUrl,
+				"tags": ["test", "test2", "tag", "test", "another tag"]
+			}
+			res.render('imageView.html', templateParameters);
+		} else { 
+			res.send(404);
+		}
+	});
 }
