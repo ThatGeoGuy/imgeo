6210d388748cf1b2d1fd943e627b16f21f77a065
Jeremy Steward <jeremy@thatgeoguy.ca>
2014-04-13 17:56:16 -0600

    Fixed query.js to use a forEach instead of raw for, implemented upload handler

--- models/query.js
+++ models/query.js
@@ -13,44 +13,64 @@ var fs   = require('fs'),
 module.exports = {};
 
 var loadQuery = function(filepath) { 
-	var query; 
-	fs.readFileSync(path.join(__dirname, filepath), function(err, data) { 
-		if(err) { 
-			throw err;
-		}
-		query = data.toString();
-	});
-	return query; 
+	var query = fs.readFileSync(filepath, 'utf-8'); 
+	// Remove all enter characters and trim the string
+	return query.split('\n').join(' ').trim(); 
 };
 
 /*
- * For each file in the queries directory, make a preparedStatement with
+ * For each file in the queries directory, make a parameterized query with
  * the same name as the file name (minus the .sql part) and return that 
  * statement. These are all put into the module.exports object, which 
  * will have a function name that is the same as the name of each of the
  * prepared statments. 
  */
-fs.readDir(path.join(__dirname, "queries"), function(err, files) { 
+var directory = path.join(__dirname, "queries"); 
+fs.readdir(directory, function(err, files) { 
 	if(err) { 
 		throw err;
 	}
 	
-	for(var i = 0; i < files.length; ++i) { 
+	files.forEach(function(el, i, array) { 
 		/*
 		 * NOTE: below each function will accept an argument 'values', 
 		 * which is an array of each of the values that need to be 
 		 * substituted into the prepared statements when they are 
 		 * constructed.
 		 */
-		module.exports[files[i].substring(0,files[i].length - 4)] = function(values) {
-			var queryString = loadQuery(path.join(__dirname, 'queries', files[i]));
-			var preparedStatement = { 
-				"name": files[i].substring(0,files[i].length - 4),
+
+		var currentFile = array[i]; 
+		var name = currentFile.substring(0,currentFile.length - 4); 
+		module.exports[name] = function(values) { 
+			var pathname = path.join(directory, currentFile); 
+			var queryString = loadQuery(pathname); 
+			var parameterizedQuery = { 
+				"text": queryString, 
+				"values": values
+			}; 
+			return parameterizedQuery; 
+		}
+	});
+
+	/*
+	for(var i = 0; i < files.length; ++i) { 
+		/*
+		 * NOTE: below each function will accept an argument 'values', 
+		 * which is an array of each of the values that need to be 
+		 * substituted into the prepared statements when they are 
+		 * constructed.
+		var currentFile = files[i].toString(); 
+		var name = currentFile.substring(0,currentFile.length - 4);
+		module.exports[name] = function(values) {
+			var pathname = path.join(directory, currentFile); 
+			var queryString = loadQuery(pathname);
+			var parameterizedQuery = { 
 				"text": queryString,
 				"values": values
 			};
-			return preparedStatement; 
+			return parameterizedQuery; 
 		};
-	}
+	};
+	*/
 });
 
--- public/js/upload.js
+++ public/js/upload.js
@@ -6,6 +6,14 @@
  * Description:  Renders the map on the upload page. 
  */
 
+$(document)
+	.on('change', '.btn-file :file', function() {
+		var input = $(this),
+		numFiles = input.get(0).files ? input.get(0).files.length : 1,
+		label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+		input.trigger('fileselect', [numFiles, label]);
+});
+
 $(document).ready(function() {
 	var initialize = function() {
 		var mapOptions = {
@@ -19,4 +27,17 @@ $(document).ready(function() {
 		var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 	}
 	initialize();
+
+	$('.btn-file :file').on('fileselect', function(event, numFiles, label) {
+		
+		var input = $(this).parents('.input-group').find(':text'),
+			log = numFiles > 1 ? numFiles + ' files selected' : label;
+		
+		if( input.length ) {
+			input.val(log);
+		} else {
+			if( log ) alert(log);
+		}
+		
+	});
 });
