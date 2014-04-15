7ce7d38181ed2a2ead4c7351a008847e1a53ac9a
Jeremy Steward <jeremy@thatgeoguy.ca>
2014-04-15 12:01:30 -0600

    Added unique constraints to database

--- models/init.sql
+++ models/init.sql
@@ -14,28 +14,30 @@ CREATE EXTENSION postgis;
 -- CREATE TABLES --
 CREATE TABLE imgeo.locations (
 	id serial PRIMARY KEY,
-	lon_lat_point geography(POINT, 4326)
+	lon_lat_point geography(POINT, 4326) UNIQUE
 );
 	
 CREATE TABLE imgeo.images ( 
 	id serial PRIMARY KEY,
 	name varchar(8000),
-	file_path varchar(41)
+	file_path varchar(41) UNIQUE 
 );
 
 CREATE TABLE imgeo.tags ( 
 	id serial PRIMARY KEY,
-	name varchar(8000)
+	name varchar(8000) UNIQUE
 );
 
 CREATE TABLE imgeo.image_tag ( 
 	id serial PRIMARY KEY,
 	image_id integer REFERENCES imgeo.images(id),
-	tag_id integer REFERENCES imgeo.tags(id)
+	tag_id integer REFERENCES imgeo.tags(id), 
+	CONSTRAINT imgeo_image_tag_unique UNIQUE (image_id, tag_id)
 );
 
 CREATE TABLE imgeo.image_location ( 
 	id serial PRIMARY KEY,
 	image_id integer REFERENCES imgeo.images(id),
-	location_id integer REFERENCES imgeo.locations(id)
+	location_id integer REFERENCES imgeo.locations(id),
+	CONSTRAINT imgeo_image_location_unique UNIQUE (image_id, location_id)
 );
